a982f4807cdcf4b2c3034578d52002a3bb3f8b77
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2018-09-02 20:39:38 -0400

    Add title and date to karma report

--- karma_plugin/views.py
+++ karma_plugin/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 from karma_plugin import KarmaPlugin
+from marvinbot.utils import localized_date
 
 karma_app = Blueprint('karma', __name__, template_folder='templates')
 
 
 @karma_app.route('/<chat_id>', methods=['GET'])
 def karmareport(chat_id):
-    return render_template('karmareport.html', report=KarmaPlugin.get_karma_report(int(chat_id)))
+    chat = getattr(karma_app, 'adapter').bot.getChat(chat_id)
+    date = localized_date()
+    return render_template('karmareport.html', title=chat.title, report=KarmaPlugin.get_karma_report(int(chat_id)), date=date)
